713745df0f5c6d3c0b8af6fae577917cb8b1116a
shikhar1est <shikharr521@gmail.com>
2025-10-04 03:06:36 +0530

    Different feed

--- single.go
+++ single.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	feedURL := "https://hnrss.org/frontpage"
+	feedURL := "https://hnrss.org/newest"
 	rss, err := fetchRSS(feedURL)
 	if err != nil {
 		fmt.Println("Error fetching RSS feed:", err)
